56f6b327bf6457ee8fe1ce3acc6847a51c592fe8
Dnyaneshwar Suryawanshi <144836277+Dnyanesh9881@users.noreply.github.com>
2024-03-06 08:04:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 //your JS code here. If required.
+addEventListener("resize", (event) => {
+	if(window.innerHeight > window.innerWidth){
+document.body.style.backgroundImage = "url('https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Paysage-en-orientation-paysage.jpg')";
+}else{
+	document.body.style.backgroundImage = "url('https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Portrait-en-orientation-portrait.jpg')";
+}
+});
+
 if(window.innerHeight > window.innerWidth){
 document.body.style.backgroundImage = "url('https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Paysage-en-orientation-paysage.jpg')";
 }else{
